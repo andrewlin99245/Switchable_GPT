87cb0909a9a5ea31068971d107d07995191a1f23
andrewlin99245 <b11901154@ntu.edu.tw>
2025-07-04 23:21:57 +0800

    update

--- trainer/step1_qat.py
+++ trainer/step1_qat.py
@@ -13,8 +13,8 @@ from ..utils import log                # your existing logger
 # Hyper-params
 BITS           = [8, 4, 2]             # target precisions (8-bit will be the teacher pass)
 SEQ_LEN        = 256                   # synthetic sequence length
-BATCH_SIZE     = 8
-STEPS          = 10000                # ≈100 k examples @ bs=2
+BATCH_SIZE     = 4
+STEPS          = 20000                # ≈100 k examples @ bs=2
 KD_WEIGHT      = 1.0                   # α in CE + α·KL
 T              = 2.0                   # soft-max temperature
 LR             = 2e-5
